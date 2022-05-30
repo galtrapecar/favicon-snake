03151e3e133188dcd793811082a503b9b6ee291a
Gal Trapečar <trapecar.gal@gmail.com>
2022-05-31 00:04:05 +0200

    Working on game_set_favicon()

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
-import dataURIs from "./dataURIs";
+import snake_face_URIs from './snake_face_URIs';
 
 function game_init() {
-    
+    let interval = game_favicon_wait_animation();
+}
+
+function game_favicon_wait_animation() {
+    let index;
+
+    let interval = setInterval(() => {
+        
+        let favicon_uri = snake_face_URIs[index].URI;
+        game_set_favicon(favicon_uri);
+
+        index = (index == snake_face_URIs.length - 1) ? 0 : index + 1;
+    }, 500);
+
+    return interval;
 }
 
 function game_animation_loop() {
 
+}
+
+function game_set_favicon(favicon_uri) {
+    let favicon_link = document.querySelector('[rel=icon]');
+    favicon.setAttribute("href", favicon_uri);
+	history.replaceState(null, null, window.location.hash == "#1" ? "#0" : "#1");
 }
