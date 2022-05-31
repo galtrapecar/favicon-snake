703fa997c146e0809047ed9398d623b9207a2793
Gal Trapečar <trapecar.gal@gmail.com>
2022-05-31 20:10:53 +0200

    Working on logic for start screen

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 import snake_face_URIs from './snake_face_URIs.js';
+import snake_press_start_URIs from './snake_press_start_URIs.js';
 
-function game_init() {
-    let interval = game_favicon_wait_animation();
+const pause = (delay) => new Promise(resolve => setTimeout(resolve, delay));
+
+// --------------------------------------------------------------
+//    ______       _       ____    ____  ________  
+//  .' ___  |     / \     |_   \  /   _||_   __  | 
+// / .'   \_|    / _ \      |   \/   |    | |_ \_| 
+// | |   ____   / ___ \     | |\  /| |    |  _| _  
+// \ `.___]  |_/ /   \ \_  _| |_\/_| |_  _| |__/ | 
+//  `._____.'|____| |____||_____||_____||________|
+// --------------------------------------------------------------
+
+async function game_init() {
+    game_waiting_animation_loop();
 }
 
-function game_favicon_wait_animation() {
-    let index = 0;
-
-    let interval = setInterval(() => {
-        let favicon_uri = snake_face_URIs[index].URI;
-        game_set_favicon(favicon_uri);
-
-        index = (index == snake_face_URIs.length - 1) ? 0 : index + 1;
-    }, 200);
-
-    return interval;
-}
-
-function game_animation_loop() {
-
+function game_waiting_animation_loop() {
+    while (true) {
+        await pause(1000);
+        await snake_blink();
+        await pause(1000);
+        await snake_press_start();
+    }
 }
 
 function game_set_favicon(favicon_uri) {
@@ -29,3 +33,30 @@ function game_set_favicon(favicon_uri) {
 }
 
 game_init();
+
+// --------------------------------------------------------------
+//   ______   ____  _____       _       ___  ____   ________  
+// .' ____ \ |_   \|_   _|     / \     |_  ||_  _| |_   __  | 
+// | (___ \_|  |   \ | |      / _ \      | |_/ /     | |_ \_| 
+//  _.____`.   | |\ \| |     / ___ \     |  __'.     |  _| _  
+// | \____) | _| |_\   |_  _/ /   \ \_  _| |  \ \_  _| |__/ | 
+//  \______.'|_____|\____||____| |____||____||____||________| 
+// --------------------------------------------------------------
+
+async function snake_blink() {
+    for (let i = 0; i < snake_face_URIs.length; i++) {
+        const URI = snake_face_URIs[i].URI;
+        game_set_favicon(URI);
+        await pause(200);
+    }
+    return;
+}
+
+async function snake_press_start() {
+    for (let i = 0; i < snake_face_URIs.length; i++) {
+        const URI = snake_face_URIs[i].URI;
+        game_set_favicon(URI);
+        await pause(200);
+    }
+    return;
+}
